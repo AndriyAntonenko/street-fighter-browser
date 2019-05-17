6a2f6860369fe1aa269d31a2358203b35bd0f3fb
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2019-05-17 21:56:42 +0300

    Add loading overlay

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,26 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 
+const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
+
 fetch(API_URL)
-  .then(response => response.json())
+  .then(response => { 
+    if( !response.ok ) {
+      return Promise.resolve(new Error('Filed rto load data...'));
+    }
+    return response.json() 
+  })
   .then(file => {
+    console.log(file);
+    
     const fighters = JSON.parse(atob(file.content));
-    console.log(fighters);
+    const names = fighters.map(fighter => fighter.name).join('\n');
+    rootElement.innerText = names;
+  })
+  .catch(err => {
+    console.warn(err);
+    rootElement.innerText = err.message;
+  })
+  .finally(() => {
+    loadingElement.remove();
   });
