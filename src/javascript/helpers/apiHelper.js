f1cdd17698622442df18030ac00e7177489b7741
AndriyAntonenko <andriyantonenko3.16@gmail.com>
2019-05-18 13:54:37 +0300

    Update apiHelper

--- src/javascript/helpers/apiHelper.js
+++ src/javascript/helpers/apiHelper.js
@@ -1,14 +1,10 @@
-const API_URL = 'https://api.github.com/';
-
-function callApi(endpoind, method) {
-  const url = API_URL + endpoind;
+function callApi(url, method) {
   const options = {
     method
   };
 
   return fetch(url, options)
     .then(response => response.ok ? response.json() : Promise.reject(Error('Failed to load')))
-    .then(file => JSON.parse(atob(file.content)))
     .catch(error => {
       console.warn(error);
       throw error;
